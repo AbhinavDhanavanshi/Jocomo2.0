18b0eb9f31f551f2515e43eb945075edee57c499
abhinav <b22ai003@iitj.ac.in>
2024-10-14 12:25:21 +0530

    asd

--- Ecom/vite.config.js
+++ Ecom/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-redux', '@reduxjs/toolkit']
+      external: ['react-toastify', 'react-redux', '@reduxjs/toolkit']
     }
   }
 });
